c0f33546d5f52914516d2fb60ed0efbdebfbb589
varun-raghuvanshi <162827843+varun-raghuvanshi@users.noreply.github.com>
2024-03-10 23:30:43 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -14,7 +14,7 @@ var hitrn=0;
  }
  function makeBubble(){
 var clutter = "";
-        for(var i = 1; i<=100; i++) {
+        for(var i = 1; i<=168; i++) {
         
          var rn = Math.floor(Math.random()*10)
         clutter += `<div class="bubble"> ${rn} </div>`;
